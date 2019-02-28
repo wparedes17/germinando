56be34424a9329a2986ab901aba8cb342bb392fd
WIlfrido J. Paredes <47059111+Wilfridovich17@users.noreply.github.com>
2019-02-28 11:57:29 -0600

    Update sql_data_mannager_alpha0.py
    Se agrega limitadamente:
    
    1. Creación de tablas
    2. Consulta de tablas
    3. Adición de registros
    4. Consulta de registros

--- sql_data_mannager_alpha0.py
+++ sql_data_mannager_alpha0.py
@@ -13,27 +13,77 @@ class db:
     def __init__(self,data_connection):
         try:
             self.db = mysql.connector.connect(**data_connection)
-            self.message = "Conexión alcanzada"
+            self.msg = "Conexión alcanzada"
             if not self.db.is_connected():
-                self.message = "Conexión no alcanzada"
+                self.msg = "Conexión no alcanzada"
         except Error as e:
-            self.message = str(e)
+            self.msg = str(e)
     #Método de creación de tablas
     #EL CREADOR DE TABLAS PODRÏA AGREGARSE A INTERFAZ MAESTRA (NO CLIENTE)
     def create_table(self):
         cursor = self.db.cursor()
         try:
-            cursor.execute("CREATE TABLE Estados (ESTADO_ID int NOT NULL AUTO_INCREMENT, ESTADO VARCHAR(255) NO NULL, PRIMARY KEY (ESTADO_ID))")
-            self.message = "Tabla creada de manera exitos"
+            cursor.execute('''CREATE TABLE Especies(
+                            ESPECIE_ID int NOT NULL AUTO_INCREMENT,
+                            ESPECIE varchar(255) NOT NULL,
+                            PRIMARY KEY (ESPECIE_ID));''')
+            self.msg = "Tabla creada de manera exitosa"
         except:
-            self.message = "Tabla no creada de manera exitosa"
+            self.msg = "Tabla no creada de manera exitosa"
+    def delete_table(self):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute("DROP TABLE Especies")
+            self.msg = "Tabla borrada de manera exitosa"
+        except:
+            self.msg = "Algo falló en la eliminación"
+    #Visor de tablas
+    def view_tablas(self):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute("SHOW TABLES;")
+            for x in cursor:
+                print(x)
+            self.msg = "Consulta de tablas exitosa"
+        except:
+            self.msg = "Algo falló en la consulta"
+    #Método de inserción de datos
+    def insert_record(self):
+        cursor = self.db.cursor()
+        species = input("Introduzca el nombre de la especie: ")
+        try:
+            cursor.execute('''INSERT INTO Especies (ESPECIE)
+            VALUES("'''+species+'");')
+            self.msg = "Registro exitoso"
+        except:
+            self.msg = "Algo falló al hacer el registro"
+    #Consulta de tablas
+    def view_records(self):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute("SELECT * FROM Especies;")
+            for x in cursor:
+                print(x)
+            self.msg = "Consulta exitosa"
+        except:
+            self.msg = "Algo falló en la consulta"
     #Método de cerradura
     def close_connection(self):
         self.db.close()
-        self.message = "Conexión terminada"
+        self.msg = "Conexión terminada"
 
 
 base_de_datos = db(datos)
-print(base_de_datos.message)
+print(base_de_datos.msg)
+#base_de_datos.create_table()
+#print(base_de_datos.msg)
+#base_de_datos.delete_table()
+#print(base_de_datos.msg)
+base_de_datos.view_tablas()
+print(base_de_datos.msg)
+#base_de_datos.insert_record()
+#print(base_de_datos.msg)
+base_de_datos.view_records()
+print(base_de_datos.msg)
 base_de_datos.close_connection()
-print(base_de_datos.message)
+print(base_de_datos.msg)
