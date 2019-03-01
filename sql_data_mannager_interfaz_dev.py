97c4a2f1acf5b3b50319ae7b60f8ea9c2d50463d
WIlfrido J. Paredes <47059111+Wilfridovich17@users.noreply.github.com>
2019-03-01 14:04:26 -0600

    Update sql_data_mannager_interfaz_dev.py
    Esto sigue en desarrollo.

--- sql_data_mannager_interfaz_dev.py
+++ sql_data_mannager_interfaz_dev.py
@@ -1,22 +1,56 @@
 import kivy
 kivy.require('1.0.5')
 
-from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.properties import ObjectProperty, NumericProperty
 
-class Controller(BoxLayout):
+import time
+import threading
 
-    screen_main = ObjectProperty()
+class ScreenOne(Screen):
+    health_points = NumericProperty(0)
 
-    def screen_bd(self):
-        self.remove_widget(self.screen_main)
+    def on_health_points(self, instance, value):
+        if value > 99:
+            self.changeScreen()
 
-class ControllerApp(App):
+    def on_enter(self, *args):
+        thread = threading.Thread(target=self.bleed)
+        thread.daemon = True
+        thread.start()
+
+    def bleed(self, *args):
+        while self.health_points < 100:
+            self.health_points += 5
+            time.sleep(0.1)
+
+    def displayScreenThenLeave(self):
+        self.changeScreen()
+
+    def changeScreen(self):
+        if self.manager.current == 'screen2':
+            self.manager.current = 'screen1'
+        else:
+            self.manager.current = 'screen2'
+
+
+class ScreenTwo(Screen):
+    def __init__(self, **kwargs):
+        super(ScreenTwo, self).__init__(**kwargs)
+
+class Manager(ScreenManager):
+
+    screen_one = ObjectProperty(None)
+    screen_two = ObjectProperty(None)
+
+
+class ScreensApp(App):
 
     def build(self):
-        return Controller()
+        m = Manager(transition=NoTransition())
+        return m
 
+if __name__ == "__main__":
+    ScreensApp().run()
 
-if __name__ == '__main__':
-    ControllerApp().run()
