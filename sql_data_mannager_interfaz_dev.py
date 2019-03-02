b2bcef4dcd5850388341cc7816d855ae1ddbf03d
Andrea-Monserrat <37232566+Andrea-Monserrat@users.noreply.github.com>
2019-03-01 21:48:05 -0600

    Update sql_data_mannager_interfaz_dev.py

--- sql_data_mannager_interfaz_dev.py
+++ sql_data_mannager_interfaz_dev.py
@@ -4,6 +4,7 @@ kivy.require('1.0.5')
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.properties import ObjectProperty, NumericProperty
+from kivy.uix.image import Image
 
 import time
 import threading
