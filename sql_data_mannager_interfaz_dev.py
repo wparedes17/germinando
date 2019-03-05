937f17e84b373a45d3262a48b70a691845bc1414
WIlfrido J. Paredes <47059111+Wilfridovich17@users.noreply.github.com>
2019-03-05 13:47:44 -0600

    Update sql_data_mannager_interfaz_dev.py
    Más de la fase de desarrollo

--- sql_data_mannager_interfaz_dev.py
+++ sql_data_mannager_interfaz_dev.py
@@ -1,83 +1,112 @@
-import kivy
-kivy.require('1.0.5')
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.properties import ObjectProperty, NumericProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.uix.popup import Popup
+from kivy.uix.textinput import TextInput
 
 import time
 import threading
+import os
 
-class ScreenOne(Screen):
-    health_points = NumericProperty(0)
+with open("SplitScreen.kv", encoding='utf8') as f:
+            Builder.load_string(f.read())
 
-    def on_health_points(self, instance, value):
-        if value > 99:
-            self.changeScreen()
+class HomeScreen(BoxLayout):
+    
+    user_name = StringProperty("Perenganito")
+            
 
+class Panel(Screen):
+    
+    user_name = StringProperty("")
+    image_path = StringProperty("")
+    
     def on_enter(self, *args):
-        thread = threading.Thread(target=self.bleed)
+        thread = threading.Thread(target=self.load_user)
         thread.daemon = True
         thread.start()
 
-    def bleed(self, *args):
-        while self.health_points < 100:
-            self.health_points += 5
-            time.sleep(0.1)
+    def load_user(self, *args):
+        archivo = open("user_info.txt","r+")
+        self.user_name = archivo.readline().strip()
+        self.image_path = archivo.readline().strip()
+        archivo.close()
+        
+class Panel2(Screen):
+    
+    user_name = StringProperty("")
+    image_path = StringProperty("non-user.jpg")
+    
+    def on_enter(self, *args):
+        thread = threading.Thread(target=self.load_user)
+        thread.daemon = True
+        thread.start()
 
-    def displayScreenThenLeave(self):
-        self.changeScreen()
+    def load_user(self, *args):
+        archivo = open("user_info.txt","r+")
+        self.user_name = archivo.readline().strip()
+        self.image_path = archivo.readline().strip()
+        archivo.close()
+        
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
-    def changeScreen(self):
-        if self.manager.current == 'screen2':
-            self.manager.current = 'screen1'
+
+class Screen1(Screen):
+    loadfile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    user_name = StringProperty("Escribe tu nombre")
+    contador = NumericProperty(0)
+    panel = StringProperty("")
+   
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        if self.contador == 0:
+            self.contador += 1
+            self.panel = "panel2"
         else:
-            self.manager.current = 'screen2'
+            self.contador = 0
+            self.panel = "panel_main"        
+        archivo = open("user_info.txt","w")
+        self.user_name = self.text_input.text
+        archivo.write(self.user_name+"\n")
+        try:
+            archivo.write(str(filename[0]))
+        except:
+            pass
+        archivo.close()
+        self.dismiss_popup()
 
-
-class ScreenTwo(Screen):
+class Screen2(Screen):
     pass
 
-class ScreenThree(Screen):
-    def __init__(self, **kwargs):
-        super(ScreenThree, self).__init__(**kwargs)
-
-
-class ScreenFour(Screen):
-
-    def __init__(self, **kwargs):
-        super(ScreenFour, self).__init__(**kwargs)
-
-    num_lines = NumericProperty(-1)
-
-    def on_enter(self, *args):
-        thread = threading.Thread(target=self.read_repository)
-        thread.daemon = True
-        thread.start()
-
-    def read_repository(self):
-        registro = open("repositorio.txt","w+")
-        registro.close()
-        self.num_lines = 1
-        if self.num_lines > 0:
-            etiqueta = Label(text="Total de fuentes encontradas: "+str(self.num_lines-1))
-            self.add_widget(etiqueta)
-
-
-class Manager(ScreenManager):
-
-    screen_one = ObjectProperty(None)
-    screen_two = ObjectProperty(None)
-    screen_three = ObjectProperty(None)
-    screen_four = ObjectProperty(None)
-
-class ScreensApp(App):
-
+class Principal(App):
     def build(self):
-        m = Manager(transition=NoTransition())
-        return m
+        return HomeScreen()
+
+
+Factory.register('Principal', cls=Principal)
+Factory.register('Panel', cls=Panel)
+Factory.register('HomeScreen', cls=HomeScreen)
+Factory.register('Screen2', cls=Screen2)
+Factory.register('Screen1', cls=Screen1)
+Factory.register('LoadDialog', cls=LoadDialog)
 
 if __name__ == "__main__":
-    ScreensApp().run()
+    Principal().run()
