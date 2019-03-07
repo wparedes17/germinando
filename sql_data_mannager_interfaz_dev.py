6495bcf850713640101586f6d8c89d8a6d2f50be
WIlfrido J. Paredes <47059111+Wilfridovich17@users.noreply.github.com>
2019-03-07 14:14:31 -0600

    Update sql_data_mannager_interfaz_dev.py
    Pantalla de usuario, funcional.

--- sql_data_mannager_interfaz_dev.py
+++ sql_data_mannager_interfaz_dev.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ListProperty
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
@@ -9,6 +9,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from kivy.event import EventDispatcher
 
 import time
 import threading
@@ -19,13 +21,23 @@ with open("SplitScreen.kv", encoding='utf8') as f:
 
 class HomeScreen(BoxLayout):
     
-    user_name = StringProperty("Perenganito")
+    def build(self):
+        sm = ScreenManager(transition=NoTransition())
+        sm_main = ScreenManager(transition=NoTransition())
+        return sm, sm_main
             
 
 class Panel(Screen):
     
     user_name = StringProperty("")
     image_path = StringProperty("")
+    
+    def __init__(self, **kwargs):
+        archivo = open("user_info.txt","r+")
+        self.user_name = archivo.readline().strip()
+        self.image_path = archivo.readline().strip()
+        archivo.close()
+        super(Panel, self).__init__(**kwargs)
     
     def on_enter(self, *args):
         thread = threading.Thread(target=self.load_user)
@@ -43,6 +55,9 @@ class Panel2(Screen):
     user_name = StringProperty("")
     image_path = StringProperty("non-user.jpg")
     
+    def __init__(self, **kwargs):
+        super(Panel2, self).__init__(**kwargs)
+    
     def on_enter(self, *args):
         thread = threading.Thread(target=self.load_user)
         thread.daemon = True
@@ -59,13 +74,20 @@ class LoadDialog(FloatLayout):
     cancel = ObjectProperty(None)
 
 
-class Screen1(Screen):
+class Screen1(Screen, EventDispatcher):
+    
     loadfile = ObjectProperty(None)
     text_input = ObjectProperty(None)
-    user_name = StringProperty("Escribe tu nombre")
+    user_name_new = StringProperty("Escribe tu nombre")
     contador = NumericProperty(0)
     panel = StringProperty("")
+    image_path_new = StringProperty("No_image")
+    load_success = StringProperty("Ninguna imagen se ha cargado")
+    label_color = ListProperty([1,0,0,1])
+
    
+    def __init__(self, **kwargs):
+        super(Screen1, self).__init__(**kwargs)
 
     def dismiss_popup(self):
         self._popup.dismiss()
@@ -77,24 +99,44 @@ class Screen1(Screen):
         self._popup.open()
 
     def load(self, path, filename):
+        try:
+            self.image_path_new = str(filename[0])
+            self.load_success = "Imagen cargada exitosamete"
+            self.label_color = [0,1,0,1]
+        except:
+            pass
+        self.dismiss_popup()
+    
+    def apply_changes(self):
+        
+        archivo = open("user_info.txt","r+")
+        self.user_name_old = archivo.readline().strip()
+        self.image_path_old = archivo.readline().strip()
+        archivo.close()
+        
+        archivo = open("user_info.txt","w")
+        self.user_name = self.text_input.text
+        if self.user_name_new != "Escribe tu nombre":
+            archivo.write(self.user_name_new+"\n")
+        else:
+            archivo.write(self.user_name_old+"\n")
+        if self.image_path_new != "No_image":
+            archivo.write(self.image_path_new)
+        else:
+            archivo.write(self.image_path_old)
+        archivo.close()
+        
         if self.contador == 0:
             self.contador += 1
             self.panel = "panel2"
         else:
             self.contador = 0
             self.panel = "panel_main"        
-        archivo = open("user_info.txt","w")
-        self.user_name = self.text_input.text
-        archivo.write(self.user_name+"\n")
-        try:
-            archivo.write(str(filename[0]))
-        except:
-            pass
-        archivo.close()
-        self.dismiss_popup()
+        
 
 class Screen2(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(Screen2, self).__init__(**kwargs)
 
 class Principal(App):
     def build(self):
