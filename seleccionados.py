1f5fe206e3c7c2c4866bed3c694f2ab8bd0bac36
Conchita983 <47086608+Conchita983@users.noreply.github.com>
2019-03-18 11:55:46 -0600

    Se cambia el color
    *Fondo de la pantalla
    *Fuente de las etiquetas

--- seleccionados.py
+++ seleccionados.py
@@ -15,7 +15,8 @@ from kivy.graphics import Color, Rectangle
 class Etiqueta(BoxLayout):
 	def __init__(self, **kwargs):
 		super(Etiqueta,self).__init__(**kwargs)
-		
+		self.cols=2
+		Window.clearcolor = (0.25, 1, 0.25, 0.9)
 		layout = GridLayout(cols=2, spacing=10, size_hint_y=None)
 		layout.bind(minimum_height=layout.setter('height'))
 		mercados=open("Nombres.txt","r")
@@ -25,8 +26,8 @@ class Etiqueta(BoxLayout):
 				registro.append(linea)
 		contador=0
 		for i in mercados:
-			layout.add_widget(Label(text="Mercado de " + i,id=str(i),size_hint_x= 0.7,size_hint_y=None, size_font = self.width*0.4,height=50))
-			layout.add_widget(Button(text=str(registro[contador]),id=str(i),size_hint_x= 0.3,size_hint_y=None, size_font = self.width*0.4,background_color=(1, .6, .2, 1),height=50))
+			layout.add_widget(Label(markup =True,text="[font=Limelight]Mercado de [/font]" + i,id=str(i),size_hint_x= 0.7,size_hint_y=None,color=(1, .3, .1, 1), size_font = self.width*0.4,height=50))
+			layout.add_widget(Button(text=str(registro[contador]),id=str(i),size_hint_x= 0.3,size_hint_y=None, size_font = self.width*0.4,background_color=(1, .6, .2, 1),color=(.7, .9, 0.7, 1),height=50))
 			contador=contador+1
 		root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
 		root.add_widget(layout)
