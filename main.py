7994116f006732bb344d3c00bbf3c99d19533b4c
Andrea-Monserrat <37232566+Andrea-Monserrat@users.noreply.github.com>
2019-05-15 15:52:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,27 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.checkbox import CheckBox
-from kivy.properties import BooleanProperty
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
-from kivy.properties import NumericProperty
-from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
 
 
 with open("Software.kv", encoding='utf8') as f:
             Builder.load_string(f.read())
 
 
-class Test(Screen):
-    password= "11235"
-    def clave(self):
-        contraseña=input()
-        if contraseña==password:
-            print(contraseña)
-        else:
-            print("error en clave")
-    def login(self, instance):
-        print(self.password.text)
-        if self.password.text == 'password':
-            popup = Popup(title='Logged',content=Label(text='Logged in successfully, admin'))
-            popup.open()
-        else:
-            popup = Popup(title='Wrong password',content=Label(text='Wrong password, admin. Go away.'))
-            popup.open()
+class Login(Screen):
 
+    def check_password(self):
+        self.password = '11235'
+        if self.ids.passw.text == str(self.password):
+            self.parent.current = 'settings'
 
 class SettingsScreen(Screen):
     pass
 
 sm = ScreenManager()
-sm.add_widget(Test(name='menu'))
+sm.add_widget(Login(name='menu'))
 sm.add_widget(SettingsScreen(name='settings'))
 
 
